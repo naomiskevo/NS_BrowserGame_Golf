2c18259ae8376aaf3b57df9cfcb0d808edbff992
naomiskevo <naomiskevo@gmsail.com>
2019-11-04 05:16:34 -0600

    Add pseudocode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-
+//assign value to all the different cards
 
 
 
@@ -14,6 +14,10 @@
 
 
 /*----- event listeners -----*/
+//Start over button starts game from Round 1
+//click deck
+//click card button 
+
 
 
 
@@ -21,3 +25,13 @@
 
 
 /*----- functions -----*/
+//init
+//Allow player to click cards 1-3 only once to see content
+//Click deck to turn over a card
+//Replace the card on top of the discard pile with whatever card is clicked from the hand
+//add all card values together when deck is finished
+//add each round score to Total Score
+//render message "Better Luck Next Time" if Total Score is > 40
+//render message "That'll Do" if Total Score is 40 - 25
+//render message "WOW, You're a pro" if Total Score is < 25
+
