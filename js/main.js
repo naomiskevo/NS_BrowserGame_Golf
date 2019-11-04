7bc7dbdb1a512bc71b488cdc602e56fcb747e086
naomiskevo <naomiskevo@gmsail.com>
2019-11-04 12:18:53 -0600

    Init function deals random 6 cards every refresh

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 /*----- constants -----*/
 //assign value to all the different cards
+const deck = [
+    1, 10, 0, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    1, 10, 0, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    1, 10, 0, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    1, 10, 0, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10
+];
+
+
 
 
 
@@ -7,11 +15,17 @@
 
 /*----- app's state (variables) -----*/
 
+let playerHand;
 
 
 
 
 
+/*----- cached element references -----*/
+let messageEl = document.querySelector('h2');
+let startOverBtn = document.getElementById('start-over');
+let hintBtn = document.getElementById('hint');
+let replayBtn = document.getElementById('replay');
 
 /*----- event listeners -----*/
 //Start over button starts game from Round 1
@@ -25,7 +39,23 @@
 
 
 /*----- functions -----*/
-//init
+function drawCard(deck) {
+    randomIndex = Math.floor(deck.length * Math.random());
+    return deck[randomIndex];
+};
+
+function init() {
+    playerHand = [
+        drawCard(deck), drawCard(deck), drawCard(deck), 
+        drawCard(deck), drawCard(deck), drawCard(deck)];
+  
+};
+
+
+
+init();
+console.log('player hand:' + playerHand);
+    // init will deal out 6 random cards from the deck
 //Allow player to click cards 1-3 only once to see content
 //Click deck to turn over a card
 //Replace the card on top of the discard pile with whatever card is clicked from the hand
